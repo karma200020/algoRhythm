1a461ccd6e38b9616d858a35ace62348c196938d
abhishek kumar mishra <39916499+karma200020@users.noreply.github.com>
2021-12-21 07:45:03 +0530

    updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = 
-[{
+[
+  {
    path: 'homepage', 
    loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) 
-}];
+  },
+  {
+    path: '', 
+    loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) 
+   },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
